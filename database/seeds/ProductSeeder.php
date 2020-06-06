ef2201040539719f3e3c3503bcded21b487959c8
Gihovani Filipp <gihovani@gmail.com>
2020-06-06 11:09:01 -0300

    new products images

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -37,25 +37,110 @@ class ProductSeeder extends Seeder
                 ]);
         }
 
+        $refrigerantes = ['Coca-cola', 'Coca-cola Zero', 'Fanta Laranja', 'Fanta Uva', 'Sprite', 'Guaraná Antartica', 'Guaraná Antartica Zero', 'Pureza'];
+        /** 2L */
         $product = factory(App\Product::class)
                 ->create([
                     'category_id' => 2,
                     'name' => 'Refrigerante 2L',
                     'pieces' => 1,
+                    'price' => 10,
                     'description' => ''
                 ]);
-        foreach (['Pureza 2L', 'Coca-cola 2L', 'Fanta 2L', 'Sprite 2L'] as $variation) {
+        foreach ($refrigerantes as $variation) {
             $product->variations()
-                ->create(['name' => $variation, 'description' => '']);
+                ->create(['name' => $variation. ' 2L', 'description' => '']);
         }
+
+        /** 1L */
+        factory(App\Product::class)
+            ->create([
+                'category_id' => 2,
+                'name' => 'Refrigerante 1L',
+                'pieces' => 1,
+                'price' => 6,
+                'description' => ''
+            ])->variations()
+            ->create(['name' => 'Pureza 1L', 'description' => '']);
+
+        /** 600 */
         $product = factory(App\Product::class)
             ->create([
                 'category_id' => 2,
-                'name' => 'Promocional Refrigerante',
+                'name' => 'Refrigerante 600ml',
                 'pieces' => 1,
-                'description' => 'Refrigerante para promoção do combo'
+                'price' => 6,
+                'description' => ''
             ]);
-        $product->variations()
+        foreach ($refrigerantes as $variation) {
+            $product->variations()
+                ->create(['name' => $variation. ' 600', 'description' => '']);
+        }
+
+        /** 350 */
+        $product = factory(App\Product::class)
+            ->create([
+                'category_id' => 2,
+                'name' => 'Refrigerante 350ml (lata)',
+                'pieces' => 1,
+                'price' => 3.5,
+                'description' => ''
+            ]);
+        foreach ($refrigerantes as $variation) {
+            $product->variations()
+                ->create(['name' => $variation. ' 350', 'description' => '']);
+        }
+
+
+        /** cervejas 350 */
+        $cervejas = ['Skol', 'Budyweiser', 'Amstel', 'Brahma', 'Antartica Subzero'];
+        $product = factory(App\Product::class)
+            ->create([
+                'category_id' => 2,
+                'name' => 'Cervejas 350ml (lata)',
+                'pieces' => 1,
+                'price' => 5,
+                'description' => ''
+            ]);
+        foreach ($cervejas as $variation) {
+            $product->variations()
+                ->create(['name' => $variation. ' 350', 'description' => '']);
+        }
+
+        /** cervejas long neck */
+        $cervejas = ['Sol', 'Budyweiser', 'Heineken', 'Eisenbahn', 'Stella Artois'];
+        $product = factory(App\Product::class)
+            ->create([
+                'category_id' => 2,
+                'name' => 'Cervejas Long Neck',
+                'pieces' => 1,
+                'price' => 7,
+                'description' => ''
+            ]);
+        foreach ($cervejas as $variation) {
+            $product->variations()
+                ->create(['name' => $variation. ' Long Neck', 'description' => '']);
+        }
+
+        /** promocionais/combos */
+        factory(App\Product::class)
+            ->create([
+                'category_id' => 2,
+                'name' => 'Refrigerante Combo 01',
+                'pieces' => 1,
+                'price' => 4.1,
+                'description' => 'Refrigerante para promoção do combo pizza grande + broto doce'
+            ])->variations()
+            ->create(['name' => 'Pureza 2L', 'description' => '']);
+
+        factory(App\Product::class)
+            ->create([
+                'category_id' => 2,
+                'name' => 'Refrigerante Combo 02',
+                'pieces' => 1,
+                'price' => 5,
+                'description' => 'Refrigerante para promoção do combo pizza grande'
+            ])->variations()
             ->create(['name' => 'Pureza 2L', 'description' => '']);
     }
 }
